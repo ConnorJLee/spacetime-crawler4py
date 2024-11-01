92a54183d4cafee392fa934d9fcf212812f1f06a
Connor Lee <connojl2@uci.edu>
2024-11-01 15:11:16 -0700

    processing changes

--- processing.py
+++ processing.py
@@ -8,7 +8,7 @@ def processWords():
     totalUrls = 0
     longest = -1
     longestUrl = ""
-    stopwords = {'any', 'their', "when's", 'themselves', "don't", 'while', "there's", 'them', 'have', 'but', 'no', 'do', 'if', 'having', "i've", 'be', 'yourself', "couldn't", 'into', 'nor', "he'd", "you're", 'are', "we'll", 'ought', "doesn't", 'under', 'they', 'too', 'our', 'down', 'all', 'did', 'and', 'both', 'does', "weren't", 'each', 'there', 'from', 'she', 'could', "they'd", "wouldn't", "hasn't", 'below', 'other', 'yourselves', 'your', 'am', 'off', 'in', "she'll", 'then', "how's", "you'd", 'this', 'most', 'being', 'few', "he's", 'not', 'he', "can't", 'as', "you'll", "wasn't", 'once', 'on', "didn't", "who's", 'that', 'myself', 'it', "hadn't", 'during', 'further', 'doing', 'how', 'of', "shan't", 'through', 'would', 'i', "they'll", 'again', 'has', "we'd", 'cannot', 'these', 'was', 'ours\tourselves', 'above', 'him', 'you', 'after', 'me', 'before', 'between', 'been', 'same', 'theirs', 'whom', "won't", "let's", 'to', 'very', 'such', 'or', 'so', "aren't", 'those', "what's", 'who', 'which', 'against', 'hers', 'over', 'by', "we're", "it's", "she'd", 'should', 'a', 'where', "she's", 'what', "haven't", 'yours', 'because', 'his', 'why', 'were', 'is', 'its', "we've", 'own', 'at', 'only', "he'll", "you've", "here's", "i'd", "i'm", 'here', "that's", "they're", 'up', 'her', 'itself', 'than', 'until', 'about', "why's", "shouldn't", 'herself', 'we', 'the', 'my', "i'll", 'out', "isn't", "where's", 'had', "they've", 'when', "mustn't", 'for', 'an', 'some', 'himself', 'more', 'with'}
+    stopwords = {'any', 'their', "when's", 'themselves', "don't", 'while', "there's", 'them', 'have', 'but', 'no', 'do', 'if', 'having', "i've", 'be', 'yourself', "couldn't", 'into', 'nor', "he'd", "you're", 'are', "we'll", 'ought', "doesn't", 'under', 'they', 'too', 'our', 'down', 'all', 'did', 'and', 'both', 'does', "weren't", 'each', 'there', 'from', 'she', 'could', "they'd", "wouldn't", "hasn't", 'below', 'other', 'yourselves', 'your', 'am', 'off', 'in', "she'll", 'then', "how's", "you'd", 'this', 'most', 'being', 'few', "he's", 'not', 'he', "can't", 'as', "you'll", "wasn't", 'once', 'on', "didn't", "who's", 'that', 'myself', 'it', "hadn't", 'during', 'further', 'doing', 'how', 'of', "shan't", 'through', 'would', 'i', "they'll", 'again', 'has', "we'd", 'cannot', 'these', 'was', 'ours', 'ourselves', 'above', 'him', 'you', 'after', 'me', 'before', 'between', 'been', 'same', 'theirs', 'whom', "won't", "let's", 'to', 'very', 'such', 'or', 'so', "aren't", 'those', "what's", 'who', 'which', 'against', 'hers', 'over', 'by', "we're", "it's", "she'd", 'should', 'a', 'where', "she's", 'what', "haven't", 'yours', 'because', 'his', 'why', 'were', 'is', 'its', "we've", 'own', 'at', 'only', "he'll", "you've", "here's", "i'd", "i'm", 'here', "that's", "they're", 'up', 'her', 'itself', 'than', 'until', 'about', "why's", "shouldn't", 'herself', 'we', 'the', 'my', "i'll", 'out', "isn't", "where's", 'had', "they've", 'when', "mustn't", 'for', 'an', 'some', 'himself', 'more', 'with'}
     with open("downloadPageReal1.txt", "r", encoding="utf8") as file:
         text = file.read()
         text = text.split("#3e5d3752-f4a1-4417-a0ea-631ac7b91200#")
@@ -38,12 +38,13 @@ def processWords():
                 longestUrl = url
                 longest = len(words)
             for word in words:
-                if word and word not in stopwords and len(word) > 1:
+                if word:
                     word = word.lower()
-                    if word not in wordDict:
-                        wordDict[word] = 1
-                    else:
-                        wordDict[word] += 1
+                    if word not in stopwords and len(word) > 1:
+                        if word not in wordDict:
+                            wordDict[word] = 1
+                        else:
+                            wordDict[word] += 1
             totalUrls += 1
 
     print(totalUrls)
@@ -54,6 +55,7 @@ def processWords():
 
 def processWorkerLog():
     longest = ["", -1]
+    longestDict = dict()
     with open("WorkerReal1.txt", "r", encoding="utf8") as file:
         for line in file:
             textWords = re.findall("NumWords=\d*", line)
@@ -61,10 +63,13 @@ def processWorkerLog():
             if textWords and not re.match(".*\.txt", line):
                 numWords = int(textWords[0][9:])
                 #numWords = int(textWords[0][8:])
+                longestDict[line] = numWords
                 if numWords > longest[1]:
                     longest[0] = line
                     longest[1] = numWords
-
+    print("Longest pages\n")
+    print(sorted(longestDict.items(), key=lambda item : item[0], reverse=True))
+    print("end")
     return longest
     
 
